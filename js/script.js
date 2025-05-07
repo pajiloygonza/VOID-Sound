8444866356bdc48463b26da91e2a42b7b952c147
Vlad <VladKiller228>
2025-05-07 19:50:25 +0200

    ferrgeg

--- js/script.js
+++ js/script.js
@@ -43,3 +43,10 @@ soundButton.classList.contains('paused') ? audio.pause() : audio.play()
 window.pause = function() {
 audio.onblur()
 }
+
+function setVH() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setVH();
+window.addEventListener("resize", setVH);
